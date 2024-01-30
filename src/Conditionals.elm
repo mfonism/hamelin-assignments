87d5f57b49d1b2d793490c62b8767d20250975ea
Mfon Eti-mfon <mfonetimfon@gmail.com>
2024-01-30 13:34:14 +0100

    Write solution to traffic light simulator question

--- src/Conditionals.elm
+++ src/Conditionals.elm
@@ -4,3 +4,8 @@ module Conditionals exposing (..)
 calculateGrade : Int -> String
 calculateGrade grade =
     "Hey hey"
+
+
+simulateTrafficLight : String -> String
+simulateTrafficLight color =
+    "Go go Brandon!"
